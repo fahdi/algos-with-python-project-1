bc5acbef0c1c7a67cb70580f8eedfab0a3306bc0
Fahad Murtaza <fahdim@gmail.com>
2019-06-16 22:39:01 +0400

    Completed Task4

--- Task4.py
+++ Task4.py
@@ -12,6 +12,42 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+list_of_numbers = []
+
+# create a list of possible telemarketers
+for call in calls:
+    if '140' in call[0]:
+        if call[0].index('140') == 0: # could use str.startswith()
+            if call[0] not in list_of_numbers:
+                list_of_numbers.append(call[0])
+
+# remove from list the numbers which also received calls
+for call in calls:
+    if '140' in call[1]:
+        if call[1].index('140') == 0:
+            if call[1] in list_of_numbers:
+                list_of_numbers.remove(call[1])
+
+# remove from list the numbers which sent texts
+for text in texts:
+    if '140' in text[0]:
+        if text[0].index('140') == 0:
+            if text[0] in list_of_numbers:
+                list_of_numbers.remove(text[0])
+    if '140' in text[1]:
+        if text[1].index('140') == 0:
+            if text[1] in list_of_numbers:
+                list_of_numbers.remove(text[1])
+
+list_of_numbers.sort(key=int)
+
+print "These numbers could be telemarketers:"
+
+#print len(list_of_numbers)
+
+for number in list_of_numbers:
+    print number
+
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
