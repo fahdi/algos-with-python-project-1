88ff7d08ce55839b879392495425910afeb56c9b
Fahad Murtaza <fahdim@gmail.com>
2019-07-03 00:08:38 +0400

    code cleanup

--- Task0.py
+++ Task0.py
@@ -3,6 +3,7 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -13,10 +14,10 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+# To print spacing correctly, I am using spacer argument to print just like in example
+print("Last record of calls, ", calls[-1][0], " calls ", calls[-1][1], " at time ", calls[-1][2], ", lasting ",
+      calls[-1][3], " seconds", sep="")
 
-print ("Last record of calls,", calls[-1][0], "calls ", calls[-1][1], "at time ", calls[-1][2], ", lasting", calls[-1][3], "seconds")
-
-", calls[-1][0], "
 """
 TASK 0:
 What is the first record of texts and what is the last record of calls?
@@ -24,4 +25,3 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-
